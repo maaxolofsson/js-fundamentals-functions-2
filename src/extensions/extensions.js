f1faeafaf0cb466478651ebcb007a059a9dd7339
Max Olofsson <max.olofsson@se.experis.com>
2024-09-23 13:23:09 +0200

    core and extension done

--- src/extensions/extensions.js
+++ src/extensions/extensions.js
@@ -4,6 +4,14 @@
 // The function must return "Phil's cake is ready!" if the remaining minutes is 0,
 // "The cake is still baking!" if there are any remaining minutes left,
 // or "You didn't set a timer!" if no value is provided to the parameter
+function timerStatus(min) {
+  if (min === 0) {
+    return "Phil's cake is ready!"
+  } else if (min > 0) {
+    return 'The cake is still baking!'
+  }
+  return "You didn't set a timer!"
+}
 
 // 2. To help Phil prepare ahead of time, create a function named estimatePrepTime
 // that accepts two parameters:
@@ -13,6 +21,11 @@
 // number of ingredients provided and the prep time per ingredient.
 // If no prep time per ingredient is provided, the function should assume each ingredient
 // takes 2 minutes to prepare
+function estimatePrepTime(ingredients, prepTime) {
+  return prepTime === undefined
+    ? ingredients.length * 2
+    : ingredients.length * prepTime
+}
 
 // 3. Phil needs to know the quantity of milk and eggs to use! Create a function
 // named calculateQuantities which accepts two parameters:
@@ -29,6 +42,12 @@
 //
 // calculateQuantities(["milk", "eggs"], 3)
 // returns: { sugar: 0, eggs: 6 }
+function calculateQuantities(ingredients, layers) {
+  return {
+    sugar: ingredients.includes('sugar') ? 100 * layers : 0,
+    eggs: ingredients.includes('eggs') ? 2 * layers : 0
+  }
+}
 
 // 4. Phil's cake is pretty small and only provides 1 portion. He wants to make a bigger one!
 // Create a function named improveRecipe that accepts two parameters:
@@ -42,7 +61,13 @@
 // Example:
 // improveRecipe({ eggs: 2, milk: 100, sugar: 200 }, 3)
 // returns: { eggs: 6, milk: 300, sugar: 600 }
-
+function improveRecipe(ingredients, portions) {
+  Object.keys(ingredients).forEach(function (key) {
+    ingredients[key] = ingredients[key] * portions
+  })
+  return ingredients
+}
+console.log(improveRecipe({ eggs: 2, milk: 100, sugar: 200 }, 3))
 // Don't change the code below this line
 module.exports = {
   timerStatus /* eslint-disable-line no-undef */,
